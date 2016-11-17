b0d5e0fe5e362f8f6692fad560eb6fc1e70b2ba9
amirch1 <amir.chervinsky@kaltura.com>
2016-11-17 21:54:41 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoaderDirective } from './directives/loader.directive';
+import { DataTableComponent } from './components/data-table/data-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoaderDirective,
+    DataTableComponent
   ],
   imports: [
     BrowserModule,
